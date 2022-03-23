2016de01a76c1693bab564122579d0fcf67d591f
rpturbina <rizki.pratama.turbina@gmail.com>
2022-03-23 18:27:23 +0700

    Add note for the instruction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 import Home from "./pages/home";
+/**
+ * TODO : Module-2-Exercise-2
+ * * From previous exercise, convert elements into components
+ * * Remember to put your components in /src/components/[your-component-name]/index.js
+ * * For <Gif> component, read this JS variable and map it as props (hint: just use url and title)
+ * * https://gist.github.com/mfaarabi/24f2aa2a8baa43567c7ddbdc270135b4
+ */
 
 function App() {
   return (
