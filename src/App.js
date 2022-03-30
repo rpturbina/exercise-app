f02b03f45cd589dcc26c683cf3c8fd023cec9b51
rpturbina <rizki.pratama.turbina@gmail.com>
2022-03-30 19:05:06 +0700

    convert from class component to func component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import "./App.css";
 // import Home from "./pages/home";
 import Search from "./pages/search";
 /**
- * TODO : Module-3-Exercise-1
- * * Create a search feature using Giphy API key created from Module 1
- * * Use the following endpoint https://developers.giphy.com/docs/api/endpoint#search
- * * Create inside directory /src/pages/search/index.js
- * * Use existing text input and search button to handle user’s event
- * * Show the first 12 results of the gifs (search for 12 gifs from the network call, not modifying the response array)
+ * TODO : Module-3-Exercise-2
+ * * Convert previous exercise from class component to functional component using hooks
  */
 
 function App() {
--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,53 +1,81 @@
-// const Search = () => (
-//   <>
-//     <form>
-//       <label htmlFor="search">Search Bar : </label>
-//       <input type="text" />
-//       <button type="button">Search</button>
-//     </form>
-//   </>
-// );
-
-import { Component } from "react";
+// import { Component, useState } from "react";
+import { useState } from "react";
 import GifImage from "../../components/Gif";
 
-class Search extends Component {
-  state = { gifs: [], searchInput: "" };
+const Search = () => {
+  const [gifs, setGifs] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-  getGifs = async (e) => {
+  const getGifs = async (event) => {
     try {
-      e.preventDefault();
+      event.preventDefault();
       const giphy_secret_key = process.env.REACT_APP_GIPHY_KEY;
       let LIMIT = 12;
-      let queryTerm = this.state.searchInput;
+      let queryTerm = searchInput;
       let endPoint = `https://api.giphy.com/v1/gifs/search?api_key=${giphy_secret_key}&q=${queryTerm}&limit=${LIMIT}`;
       const gifs = await fetch(endPoint).then((response) => response.json());
       // console.log(gifs.data);
-      this.setState({ gifs: gifs.data });
+      setGifs(gifs.data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  handleInput = (e) => this.setState({ searchInput: e.target.value });
+  const handleInput = (event) => setSearchInput(event.target.value);
 
-  render() {
-    const { gifs } = this.state;
-    return (
-      <div className="search">
-        <form>
-          <label htmlFor="search">Search Bar : </label>
-          <input type="text" onChange={this.handleInput} value={this.state.searchInput} />
-          <button type="submit" onClick={this.getGifs}>
-            Search
-          </button>
-        </form>
-        {gifs.map((gif) => (
-          <GifImage key={gif.id} src={gif.images.fixed_width.url} title={gif.title} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search">
+      <form>
+        <label htmlFor="search">Search Bar : </label>
+        <input type="text" onChange={handleInput} value={searchInput} />
+        <button type="submit" onClick={getGifs}>
+          Search
+        </button>
+      </form>
+      {gifs.map((gif) => (
+        <GifImage key={gif.id} src={gif.images.fixed_width.url} title={gif.title} />
+      ))}
+    </div>
+  );
+};
+
+// class Search extends Component {
+//   state = { gifs: [], searchInput: "" };
+
+//   getGifs = async (e) => {
+//     try {
+//       e.preventDefault();
+//       const giphy_secret_key = process.env.REACT_APP_GIPHY_KEY;
+//       let LIMIT = 12;
+//       let queryTerm = this.state.searchInput;
+//       let endPoint = `https://api.giphy.com/v1/gifs/search?api_key=${giphy_secret_key}&q=${queryTerm}&limit=${LIMIT}`;
+//       const gifs = await fetch(endPoint).then((response) => response.json());
+//       // console.log(gifs.data);
+//       this.setState({ gifs: gifs.data });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   handleInput = (e) => this.setState({ searchInput: e.target.value });
+
+//   render() {
+//     const { gifs } = this.state;
+//     return (
+//       <div className="search">
+//         <form>
+//           <label htmlFor="search">Search Bar : </label>
+//           <input type="text" onChange={this.handleInput} value={this.state.searchInput} />
+//           <button type="submit" onClick={this.getGifs}>
+//             Search
+//           </button>
+//         </form>
+//         {gifs.map((gif) => (
+//           <GifImage key={gif.id} src={gif.images.fixed_width.url} title={gif.title} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 export default Search;
